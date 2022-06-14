3549a77cefc05d7c31353a1e49c079d0d255f388
Dr. A <4411916+scotpatti@users.noreply.github.com>
2022-06-14 14:20:57 -0400

    Add project files.

---
+++ LoginTest/Areas/Identity/Data/LoginTestContext.cs
@@ -0,0 +1,22 @@
+﻿using LoginTest.IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginTest.Data;
+
+public class LoginTestContext : IdentityDbContext<User>
+{
+    public LoginTestContext(DbContextOptions<LoginTestContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ LoginTest/IdentityModel/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LoginTest.IdentityModel
+{
+    public class User : IdentityUser
+    {
+        // See https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.identity.entityframeworkcore.identityuser?view=aspnetcore-1.1
+        // for IdenityUser properties that are included in IdentityUser.
+
+        //You can add anything you want to have about your user here e.g.:
+        public string MaidenName { get; set; }
+
+    }
+}
